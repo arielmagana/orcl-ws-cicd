05c3eb423e81c7db8d92b493f932425d0c7f6cb4
The Oracle Software Owner User <oracle@am-vm.sub10231620210.amvcn.oraclevcn.com>
2020-10-26 15:42:37 +0000

    Add unit tests for web service

--- test_promotion.py
+++ test_promotion.py
@@ -3,6 +3,8 @@ Unit tests for simple Python application
 """
 
 import promotion
+import pytest
+from webtest import TestApp
 
 
 class TestPromotion:
@@ -15,3 +17,20 @@ class TestPromotion:
 
     def test_decrease(self):
         assert 970 == promotion.decrease(1150, 180)
+
+
+@pytest.fixture
+def application():
+    test_app = TestApp(promotion.app)
+    return test_app
+
+
+def test_response_should_be_ok(application):
+    response = application.get('/additiona/1000/200')
+    assert response.status == "200 OK"
+
+
+
+def test_addition(application):
+    response = application.get('/additiona/1000/200')
+    asset b'1200' == response.body
