a7066603be38393e1a9aa8268071f6d9d6077e0f
The Oracle Software Owner User <oracle@am-vm.sub10231620210.amvcn.oraclevcn.com>
2020-10-26 17:38:07 +0000

    Add database connection unit test

--- test_promotion.py
+++ test_promotion.py
@@ -5,6 +5,7 @@ Unit tests for simple Python application
 import promotion
 import pytest
 from webtest import TestApp
+import cx_Oracle
 
 
 class TestPromotion:
@@ -33,3 +34,16 @@ def test_response_should_be_ok(application):
 def test_addition(application):
     response = application.get('/addition/1000/200')
     assert b'1200' == response.body
+
+
+@pytest.fixture(scope='session')
+def test_connection():
+    DBUSER = 'hr'
+    DBPASS = 'WelCom3#2020_'
+    DBHOST = 'am-host-scan.sub10231620210.amvcn.oraclevcn.com'
+    DBSERV = 'pdb01.sub10231620210.amvcn.oraclevcn.com'
+    conn_string = DBUSER + '/' + DBPASS + '@//' + DBHOST + '/' + DBSERV
+    connection = cx_Oracle.connect(conn_string)
+    response = connection.version
+    assert response == '19.8.0.0.0'
+    connection.close()
